0aeb5755fbbab116c21c4f5df7dd025f2c8f23e4
nightwebworm <nightwebworm@gmail.com>
2022-04-26 19:43:54 +0300

    no a path sol try

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ window.onload = ()=>{
     solution.addEventListener('click',()=>{
         sudoku = getgrid(size)
         console.log(sudoku)
-        findSolution(sudoku)
-    })
+        window.s = sudoku
+
+        const newsudoku = findSolution(sudoku);
+        console.log(newsudoku)
+
+    })    
 
 }
 
@@ -42,15 +46,107 @@ function getgrid(size){
         if (sudoku[y]==undefined){
             sudoku[y] = []
         }
-        sudoku[y].push(el.value)
+        if (el.value == ''){
+            sudoku[y].push(0)
+        } else{
+            sudoku[y].push(parseInt(el.value))
+        }
+        
     });
     return sudoku;
 }
     
 function findSolution(sudoku){
+    let newsudoku = copySudoku(sudoku)
+    try{
+        sudoku.forEach( (row,y)=>{
+            row.forEach((val,x)=>{
+                if(sudoku[y][x]==0){
+                    let children = findChildren(newsudoku,y,x);
+                    if (children.length==0){
+                        console.log('end: ', newsudoku);
+                        throw 'Break';
+                    } 
+                    newsudoku = children[0]
+                }
+            })
+        } )
+    } catch(e){
+        
+    }
+    return newsudoku;
+    
 
+    
 }
 
 function findChildren(sudoku,y,x){
-    
+    if (sudoku[y][x]!=0){
+        return -1
+    }
+
+    const children = [];
+
+    let flag = false;
+    for (let i=1;i<=9;i++){
+        flag = false;
+
+        // row check
+        if (sudoku[y].includes(i)){
+            flag = true;
+            //continue;
+        }
+
+        // column check
+        for (let row=0;row<9;row++){
+            if (sudoku[row][x]==i){
+                flag = true;
+               // break;
+            }
+        }
+        // if (flag) continue;
+
+        // square check
+        if (x == 6 && y ==1){
+            console.log(":")
+        }
+        let sqxstart = 3*Math.floor(x/3);
+        let sqystart = 3*Math.floor(y/3);
+
+        for (let sqy=sqystart;sqy<sqystart+3;sqy++){
+            // if (flag)break;
+            for (let sqx=sqxstart;sqx<sqxstart+3;sqx++){
+                if (x == 6 && y ==1){
+                    console.log(":",sqy, sqx)
+                }
+                if (sudoku[sqy][sqx]==i){
+                    flag = true;
+                }
+            }
+        }
+
+        if (!flag){
+            const newsudoku = copySudoku(sudoku)
+            newsudoku[y][x] = i;
+            children.push(newsudoku)
+        };
+
+    }
+    if (children.length==0) console.log('p', y,x)
+    return children;
+}
+
+window.f = findChildren
+
+function copySudoku(sudoku){
+    const newSudoku = []
+    sudoku.forEach( (row,y)=>{
+        row.forEach((val,x)=>{
+            if (newSudoku[y]==undefined){
+                newSudoku[y] = []
+            };
+            newSudoku[y][x] = val;
+        })
+    } )
+    return newSudoku;
 }
